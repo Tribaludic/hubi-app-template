1aa1295dd76522bbab3c330ff39d4660634fbd80
Christian Bustamante <58147749+Tribaludic@users.noreply.github.com>
2023-03-28 12:21:28 -0500

    remove unused components

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import LeftMenu from '../../components/left_menu/LeftMenu';
 import GlobalAppBar from '../../components/app_bar/GlobalAppBar';
 
 const Home = () => {
-    const [leftDrawer, setLeftDrawerState] = React.useState(false);
-
     return (
         <div>
-            <LeftMenu state={leftDrawer} setState={setLeftDrawerState}/>
             <GlobalAppBar title={'HOME'}/>
         </div>
     )
--- src/pages/page2/Page2.js
+++ src/pages/page2/Page2.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import LeftMenu from '../../components/left_menu/LeftMenu';
 import GlobalAppBar from '../../components/app_bar/GlobalAppBar';
 
 const Page2 = () => {
-    const [leftDrawer, setLeftDrawerState] = React.useState(false);
     return (
         <div>
-            <LeftMenu state={leftDrawer} setState={setLeftDrawerState}/>
             <GlobalAppBar title={'PAGE 2'}/>
         </div>
     )
